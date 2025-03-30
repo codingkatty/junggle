bb7fa5fd7bcd72dd6d0c956827afe34f18cba72b
codingkatty <m001720@permatapintar.ukm.edu.my>
2025-03-30 08:55:03 +0800

    generation part 1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, moveY, 0f);
+        transform.position += movement * 5f * Time.deltaTime;
+    }
+}
